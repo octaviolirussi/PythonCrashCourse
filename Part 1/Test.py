8e13b272bffd598b6a80c3d916998484af880a6f
OctavioLirussi <octaviolirussi@gmail.com>
2025-09-09 12:43:37 -0300

    09/09/2025

--- Part 1/Test.py
+++ Part 1/Test.py
@@ -1,4 +1,7 @@
-motorcycles = ['honda', 'yamaha', 'suzuki']
-print(motorcycles)
-del motorcycles[0]
-print(motorcycles)
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+# del motorcycles[0]
+# print(motorcycles)
+
+squares = [value for value in range(1, 11)]
+print(squares)
